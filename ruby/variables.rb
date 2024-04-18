1b6a4f4fd451007f488cfc800138e3b4c85dac43
Sammy Torres <storresii1316@gmail.com>
2024-04-18 12:28:27 -0400

    completed variables

---
+++ ruby/variables.rb
@@ -0,0 +1,35 @@
+# Variables
+
+## Declaring a Variable
+
+age = 18
+p age
+
+    ## Assign Expressions to a Variable
+age = 18 + 5
+p age
+
+    ## Reassign a Variable
+age = age + 4
+p age
+
+    ## Shorthand Assignment Operatiors
+        ## Example: +=, -=, *=, and /=
+age += 4
+p age
+
+age -= 2
+p age
+
+cash = 10
+cash *= 2
+p cash
+
+temp = 40
+temp /= 10
+p temp
+
+    ## Naming Variables
+        # names should be accurate and descriptive
+        # should be in lowercase
+        # multiple words should be in snake_case
