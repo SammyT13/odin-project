56121ff9fc47eaf15f56eef888be322089b9cf89
Sammy Torres <storresii1316@gmail.com>
2024-04-30 11:59:45 -0400

    completed input output module

---
+++ ruby/input_output.rb
@@ -0,0 +1,13 @@
+# Input and Output
+
+print "Learning to code is FUN!"
+
+puts "Learning to code is FUN!"
+
+    ## Input Commands
+        # We use "gets" command to get user input
+        # We also use the "chomp" method to remove new line
+        # we can assign gets.chomp to a variable
+
+new_string = gets.chomp
+puts new_string
